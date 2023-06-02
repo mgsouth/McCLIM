4ad3a47df08ef877d99b5df0e04be7611037c528
Daniel Kochmański <daniel@turtleware.eu>
2023-06-02 11:01:34 +0200

    extensions: slim: recompiling apps redisplays them

--- Libraries/Slim/slim.lisp
+++ Libraries/Slim/slim.lisp
@@ -1,80 +1,13 @@
 (defpackage #:slim
   (:use #:clim-lisp)
   (:export #:+golden-ratio+
-           #:defapp
-           #:with-table #:row #:col #:cell #:*pane*
-           #:parent #:children :add-child :delete-child :repaint))
+           #:parent #:children :add-child :delete-child :repaint
+           #:defapp #:display
+           #:with-table #:row #:col #:cell #:*pane*))
 (in-package #:slim)
 
 (defparameter +golden-ratio+ #. (/ (+ 1 (sqrt 5)) 2)
   "Golden Ratio constant.")
-
-
-;;; application frames
-(defmacro defapp (name-and-opts args &body body)
-  "Macro allows defining a single-pane application frame as if it were
-a function.
-
-NAME-AND-OPTS is a list where first element is a symbol being the
-frame and the function name. The rest of a list are arguments passed
-to the application pane as the initialization arguments. NAME-AND-OPTS
-may be a symbol, then it is treated as a list with a single element.
-
-Args are function arguments. They are available in the body. Bindings
-are closed over the display function.
-
-Examples:
-
-  (defapp example-1 (x y z)
-    (clim:draw-rectangle* *standard-output* x y (+ x 100) (+ y 200))
-    (format t \"~&Z is ~s\" z))
-
-  (defapp (example-2 :text-margins `(:left 20 :top 10 :right 20 :bottom 10))
-      (x y z)
-    (clim:draw-rectangle* *standard-output* x y (+ x 100) (+ y 200))
-    (format t \"~&Z is ~s\" z))
-
-  (example-1 100 100 \"jackdaniel\")
-  (example-2  50  50 \"another\")
-"
-  (alexandria:with-gensyms (display-fn)
-    (destructuring-bind (name &rest pane-options)
-        (alexandria:ensure-list name-and-opts)
-      `(progn
-         (clim:define-application-frame ,name ()
-           ((,display-fn :initarg :display-function
-                         :reader display-function))
-           (:menu-bar nil)
-           (:pane :application
-            :display-function (display-function clim:*application-frame*)
-            ,@pane-options))
-         (defun ,name ,args
-           (flet ((,display-fn (clim:*application-frame* *standard-output*)
-                    ,@body))
-             (declare (dynamic-extent #',display-fn))
-             (clim:run-frame-top-level
-              (clim:make-application-frame ',name :display-function #',display-fn))))))))
-
-
-;;; context
-(defvar *pane*)
-
-(defmacro with-pane ((pane) &body body)
-  `(let* ((*pane* ,pane)
-          (*standard-output* *pane*))
-     ,@body))
-
-
-;;; with-table
-(defmacro with-table ((&optional (pane *pane*) &rest options)
-                      &body body)
-  `(with-pane (,pane)
-     (clim:formatting-table (*pane* ,@options)
-       ,@body)))
-
-(defmacro row  (&body body) `(clim:formatting-row    (*pane*) ,@body))
-(defmacro col  (&body body) `(clim:formatting-column (*pane*) ,@body))
-(defmacro cell (&body body) `(clim:formatting-cell   (*pane*) ,@body))
 
 
 #| Major issue: There is a proposal on the table to unify the sheet and output
@@ -121,6 +54,79 @@ formatting. --- York, SWM |#
 
 (defgeneric repaint (object target region)
   (:method ((sheet clim:sheet) medium region)
+    (declare (ignore medium))
     (clim:repaint-sheet sheet region))
   (:method ((output-record clim:output-record) sheet region)
     (clim:replay-output-record output-record sheet region)))
+
+
+;;; It would be nice if we had a canonical function DISPLAY that may be
+;;; specialized on both the frame and the stream and passed uniformly as
+;;; #'DISPLAY. That's what we do here.
+(defgeneric display (frame stream)
+  (:method (frame stream)
+    (format stream "~a" frame)))
+
+;;; application frames
+(defmacro defapp (name-and-opts args &body body)
+  "Macro allows defining a single-pane application frame as if it were
+a function.
+
+NAME-AND-OPTS is a list where first element is a symbol being the
+frame and the function name. The rest of a list are arguments passed
+to the application pane as the initialization arguments. NAME-AND-OPTS
+may be a symbol, then it is treated as a list with a single element.
+
+Args are function arguments. They are available in the body. Bindings
+are closed over the display function.
+
+Examples:
+
+  (defapp example-1 (x y z)
+    (clim:draw-rectangle* *standard-output* x y (+ x 100) (+ y 200))
+    (format t \"~&Z is ~s\" z))
+
+  (defapp (example-2 :text-margins `(:left 20 :top 10 :right 20 :bottom 10))
+      (x y z)
+    (clim:draw-rectangle* *standard-output* x y (+ x 100) (+ y 200))
+    (format t \"~&Z is ~s\" z))
+
+  (example-1 100 100 \"jackdaniel\")
+  (example-2  50  50 \"another\")
+"
+  (destructuring-bind (name &rest pane-options)
+      (alexandria:ensure-list name-and-opts)
+    `(progn
+       (clim:define-application-frame ,name ()
+         ((app-args :initarg :app-args :reader app-args))
+         (:menu-bar nil)
+         (:reinitialize-frames t)
+         (:pane :application
+          :display-function #'display
+          ,@pane-options))
+       (defmethod display ((clim:*application-frame* ,name) *standard-output*)
+         (destructuring-bind ,args (app-args clim:*application-frame*)
+           ,@body))
+       (defun ,name (&rest args)
+         (clim:find-application-frame ',name :app-args args :create :force)))))
+
+
+;;; context
+(defvar *pane*)
+
+(defmacro with-pane ((pane) &body body)
+  `(let* ((*pane* ,pane)
+          (*standard-output* *pane*))
+     ,@body))
+
+
+;;; with-table
+(defmacro with-table ((&optional (pane *pane*) &rest options)
+                      &body body)
+  `(with-pane (,pane)
+     (clim:formatting-table (*pane* ,@options)
+       ,@body)))
+
+(defmacro row  (&body body) `(clim:formatting-row    (*pane*) ,@body))
+(defmacro col  (&body body) `(clim:formatting-column (*pane*) ,@body))
+(defmacro cell (&body body) `(clim:formatting-cell   (*pane*) ,@body))
