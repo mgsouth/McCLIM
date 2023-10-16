841b0d043ce6c0d50dc7120037693ef4d968850f
Daniel Kochmański <daniel@turtleware.eu>
2023-10-16 15:23:14 +0200

    core: set the port keyboard input focus when the port exists
    There was a race condition: if the user closes the window while the operation
    happens, then the sheet may get degrafted, and only after that the window
    manager focus event comes.

--- Core/windowing/ports.lisp
+++ Core/windowing/ports.lisp
@@ -382,7 +382,8 @@
 ;;; We focus a sheet in the CLIM thread.
 (defmethod handle-event ((sheet top-level-sheet-mixin)
                          (event window-manager-focus-event))
-  (setf (port-keyboard-input-focus (port sheet)) sheet))
+  (when-let ((port (port sheet)))
+    (setf (port-keyboard-input-focus port) sheet)))
 
 ;;; In the most general case we can't tell whether all sheets are mirrored or
 ;;; not. So this default method for pointer-events operates under the
