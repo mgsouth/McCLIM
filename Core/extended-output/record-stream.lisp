4f4094ce1aa2c26f3dbab13f0850ea2954864422
Daniel Kochmański <daniel@turtleware.eu>
2024-01-12 14:33:41 +0100

    seos: change the cursor position /after/ wrapping
    The previous version made the cursor bigger before the line break, so if the
    bigger element was largest, we had too big line.

--- Core/extended-output/record-stream.lisp
+++ Core/extended-output/record-stream.lisp
@@ -232,8 +232,7 @@ add output recording facilities. It is not instantiable."))
                                     width height baseline))
 
 (defun stream-add-record-output (stream record width height base-x base-y)
-  (add-object-to-text-record (stream-text-output-record stream nil)
-                             record width height base-x base-y))
+  (add-object-to-text-record (stream-text-output-record stream nil) record))
 
 ;;; Text output catching methods
 (defmethod stream-write-output
