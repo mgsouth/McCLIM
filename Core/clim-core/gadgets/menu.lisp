f17799fbeffc26a63dd65d45d7efa3f9d7d39c9f
Daniel Kochmański <daniel@turtleware.eu>
2024-02-16 11:11:32 +0100

    core: menu: don't rely on {de}activate-gadget when command renabled
    When the sheet is degrafted and engrafted again, this state will be wiped
    clean. So in order to preserve it we directly call COMMAND-ENABLED on the
    command name for the gadget's frame.

--- Core/clim-core/gadgets/menu.lisp
+++ Core/clim-core/gadgets/menu.lisp
@@ -54,25 +54,10 @@
                    (reverse (sheet-children rack)))))
 
 ;;; This function is called when a command has been enabled or disabled for the
-;;; frame. We assume that the command is accessible in the command table, so we
-;;; skip calling COMMAND-ENABLED. Also we traverse all leaf buttons in case the
-;;; same command is assigned to multiple buttons. -- jd 2021-03-03
+;;; frame. -- jd 2021-03-03
 (defun menu-bar-refresh-command (frame command-name enabledp)
-  (labels ((rack (client)
-             (and (typep client 'menu-button-submenu-pane)
-                  (submenu-pane client)))
-           (cmdp (client)
-             (and (typep client 'menu-button-leaf-pane)
-                  (eq command-name (slot-value client 'command-name))))
-           (map-leaf-buttons (rack)
-             (when rack
-               (loop for child in (sheet-children rack)
-                     do (if (cmdp child)
-                            (if enabledp
-                                (activate-gadget child)
-                                (deactivate-gadget child))
-                            (map-leaf-buttons (rack child)))))))
-    (map-leaf-buttons (frame-menu-bar-pane frame))))
+  (declare (ignore command-name enabledp))
+  (dispatch-repaint (frame-menu-bar-pane frame) +everywhere+))
 
 (define-gesture-name :menu-exit  :keyboard (:escape))
 (define-gesture-name :menu-left  :keyboard (:left))
@@ -244,6 +229,11 @@
 
 (defclass menu-button-leaf-pane (menu-button-pane)
   ((command-name :initform nil :initarg :command-name)))
+
+(defmethod gadget-active-p ((gadget menu-button-leaf-pane))
+  (and (command-enabled (slot-value gadget 'command-name)
+                        (pane-frame gadget))
+       (call-next-method)))
 
 (defclass menu-divider-leaf-pane (sheet-leaf-mixin basic-gadget)
   ((label :initform nil :initarg :label)))
