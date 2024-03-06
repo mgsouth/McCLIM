4b6927e3057da66adfb44f4294334d4b92d5dff4
Daniel Kochmański <daniel@turtleware.eu>
2024-03-06 12:19:45 +0100

    clx: implement directional text rendering

--- Backends/CLX/fonts-truetype.lisp
+++ Backends/CLX/fonts-truetype.lisp
@@ -32,8 +32,8 @@
   (declare (ignore font code direction transformation))
   (let* ((info (call-next-method))
          (pixarray (glyph-info-pixarray info))
-         (x1 (glyph-info-left info))
-         (y1 (glyph-info-top info))
+         (x0 (glyph-info-origin-x info))
+         (y0 (glyph-info-origin-y info))
          (dx (glyph-info-advance-dx info))
          (dy (glyph-info-advance-dy info)))
     (when (= (array-dimension pixarray 0) 0)
@@ -48,7 +48,7 @@
           (glyph-id (draw-glyph-id port)))
       (xlib:render-add-glyph glyph-set glyph-id
                              :data pixarray
-                             :x-origin (- x1) :y-origin y1
+                             :x-origin x0 :y-origin y0
                              :x-advance dx :y-advance dy)
       (setf (glyph-info-id info) glyph-id))
     info))
@@ -61,46 +61,197 @@
 
 (defvar *draw-font-lock* (clim-sys:make-lock "draw-font"))
 
-(declaim (inline font-prepare-glyphs))
 (defun font-prepare-glyphs (glyph-ids font string start end
                             x y align-x align-y transform-glyphs)
   (declare (optimize (speed 3))
            (type index start end)
-           (type string string)
-           (ignore transform-glyphs))
+           (type string string))
+  (ecase transform-glyphs
+    (:left-to-right
+     (font-prepare-glyphs/ltr
+      glyph-ids font string start end x y align-x align-y transform-glyphs))
+    (:right-to-left
+     (font-prepare-glyphs/rtl
+      glyph-ids font string start end x y align-x align-y transform-glyphs))
+    (:top-to-bottom
+     (font-prepare-glyphs/ttb
+      glyph-ids font string start end x y align-x align-y transform-glyphs))
+    (:bottom-to-top
+     (font-prepare-glyphs/btt
+      glyph-ids font string start end x y align-x align-y transform-glyphs))))
+
+(defun font-prepare-glyphs/ltr (glyph-ids font string start end
+                                x y align-x align-y transform-glyphs)
+  (declare (optimize (speed 3))
+           (type index start end)
+           (type string string))
   (clim-sys:with-lock-held (*draw-font-lock*)
-    (loop with origin-x = nil
-          with origin-y = (font-ascent font)
-          with advance-x = 0
-          with advance-y = (font-descent font)
-          with this-char = (char string start)
-          with idx0 of-type index = 0
-          for idx1 of-type index from (1+ start) below end
-          as next-char = (char string idx1)
-          as code = (char-glyph-code this-char next-char)
-          as glyph = (font-glyph-info font code :left-to-right)
-          do
-             (when (null origin-x)
-               (setf origin-x (- (glyph-info-left glyph))))
-             (setf (aref (the (simple-array (unsigned-byte 32)) glyph-ids) idx0)
-                   (the (unsigned-byte 32) (glyph-info-id glyph)))
-             (setf this-char next-char)
-             (incf idx0)
-             (incf advance-x (glyph-info-advance-dx glyph))
-          finally
-             (setf glyph (font-glyph-info font (char-code this-char) :left-to-right))
-             (when (null origin-x)
-               (setf origin-x (- (glyph-info-left glyph))))
-             (setf (aref (the (simple-array (unsigned-byte 32)) glyph-ids) idx0)
-                   (the (unsigned-byte 32) (glyph-info-id glyph)))
-             (incf advance-x (glyph-info-advance-dx glyph))
-             (return (values origin-x origin-y advance-x advance-y
-                             (ecase align-x
-                               (:left x)
-                               (:center (decf x (+ (/ advance-x 2.0))))
-                               (:right  (decf x advance-x)))
-                             (ecase align-y
-                               (:baseline y)
-                               (:top    (incf y origin-y))
-                               (:center (incf y (/ (- origin-y advance-y) 2.0)))
-                               (:bottom (decf y advance-y))))))))
+    (let ((firstp t) xmin ymin xmax ymax advance-x)
+      (flet ((process-code (code index)
+               (let ((info (font-glyph-info font code transform-glyphs)))
+                 (when firstp
+                   (setf firstp nil
+                         xmin x
+                         ymin (- y (font-ascent font))
+                         ymax (+ y (font-descent font))
+                         advance-x 0))
+                 (incf advance-x (glyph-info-advance-dx info))
+                 (setf (aref (the (simple-array (unsigned-byte 32)) glyph-ids) index)
+                       (the (unsigned-byte 32) (glyph-info-id info))))))
+        (declare (inline process-code))
+        (loop with this-char = (char string start)
+              with idx0 of-type index = 0
+              for idx1 of-type index from (1+ start) below end
+              as next-char = (char string idx1)
+              as code = (char-glyph-code this-char next-char)
+              do (process-code code idx0)
+                 (setf this-char next-char)
+                 (incf idx0)
+              finally
+                 (process-code (char-code this-char) idx0)
+                 (setf xmax (+ xmin advance-x))))
+      (let ((dx (ecase align-x
+                  (:left   0)
+                  (:center (- (/ (- xmax xmin) 2.0)))
+                  (:right  (- (- xmax xmin)))))
+            (dy (ecase align-y
+                  (:baseline 0)
+                  (:center (- y (/ (+ ymax ymin) 2.0)))
+                  (:top    (- y ymin))
+                  (:bottom (- y ymax)))))
+        (incf x dx) (incf xmin dx) (incf xmax dx)
+        (incf y dy) (incf ymin dy) (incf ymax dy))
+      (values x y xmin ymin xmax ymax))))
+
+(defun font-prepare-glyphs/ttb (glyph-ids font string start end
+                                x y align-x align-y transform-glyphs)
+  (declare (optimize (speed 3))
+           (type index start end)
+           (type string string))
+  (clim-sys:with-lock-held (*draw-font-lock*)
+    (let ((firstp t) xmin ymin xmax ymax advance-y
+          (line-height (+ (font-descent font)
+                          (font-ascent font))))
+      (flet ((process-code (code index)
+               (let ((info (font-glyph-info font code transform-glyphs)))
+                 (when firstp
+                   (setf firstp nil
+                         ymin y
+                         xmin (- x (/ line-height 2))
+                         xmax (+ x (/ line-height 2))
+                         advance-y 0))
+                 (incf advance-y (glyph-info-advance-dy info))
+                 (setf (aref (the (simple-array (unsigned-byte 32)) glyph-ids) index)
+                       (the (unsigned-byte 32) (glyph-info-id info))))))
+        (declare (inline process-code))
+        (loop with this-char = (char string start)
+              with idx0 of-type index = 0
+              for idx1 of-type index from (1+ start) below end
+              as next-char = (char string idx1)
+              as code = (char-glyph-code this-char next-char)
+              do (process-code code idx0)
+                 (setf this-char next-char)
+                 (incf idx0)
+              finally
+                 (process-code (char-code this-char) idx0)
+                 (setf ymax (+ ymin advance-y))))
+      (let ((dy (ecase align-x
+                  (:left   0)
+                  (:center (- (/ (- ymax ymin) 2.0)))
+                  (:right  (- (- ymax ymin)))))
+            (dx (ecase align-y
+                  (:baseline 0)
+                  (:center (- x (/ (+ xmax xmin) 2.0)))
+                  (:top    (- x xmax))
+                  (:bottom (- x xmin)))))
+        (incf x dx) (incf xmin dx) (incf xmax dx)
+        (incf y dy) (incf ymin dy) (incf ymax dy))
+      (values x y xmin ymin xmax ymax))))
+
+(defun font-prepare-glyphs/rtl (glyph-ids font string start end
+                                x y align-x align-y transform-glyphs)
+  (declare (optimize (speed 3))
+           (type index start end)
+           (type string string))
+  (clim-sys:with-lock-held (*draw-font-lock*)
+    (let ((firstp t) xmin ymin xmax ymax advance-x)
+      (flet ((process-code (code index)
+               (let ((info (font-glyph-info font code transform-glyphs)))
+                 (when firstp
+                   (setf firstp nil
+                         xmax x
+                         ymin (- y (font-ascent font))
+                         ymax (+ y (font-descent font))
+                         advance-x 0))
+                 (incf advance-x (glyph-info-advance-dx info))
+                 (setf (aref (the (simple-array (unsigned-byte 32)) glyph-ids) index)
+                       (the (unsigned-byte 32) (glyph-info-id info))))))
+        (declare (inline process-code))
+        (loop with this-char = (char string start)
+              with idx0 of-type index = 0
+              for idx1 of-type index from (1+ start) below end
+              as next-char = (char string idx1)
+              as code = (char-glyph-code this-char next-char)
+              do (process-code code idx0)
+                 (setf this-char next-char)
+                 (incf idx0)
+              finally
+                 (process-code (char-code this-char) idx0)
+                 (setf xmin (+ xmax advance-x))))
+      (let ((dx (ecase align-x
+                  (:left   (- xmax xmin))
+                  (:center (+ (/ (- xmax xmin) 2.0)))
+                  (:right  0)))
+            (dy (ecase align-y
+                  (:baseline 0)
+                  (:center (- y (/ (+ ymax ymin) 2.0)))
+                  (:top    (- y ymin))
+                  (:bottom (- y ymax)))))
+        (incf x dx) (incf xmin dx) (incf xmax dx)
+        (incf y dy) (incf ymin dy) (incf ymax dy))
+      (values x y xmin ymin xmax ymax))))
+
+(defun font-prepare-glyphs/btt (glyph-ids font string start end
+                                x y align-x align-y transform-glyphs)
+  (declare (optimize (speed 3))
+           (type index start end)
+           (type string string))
+  (clim-sys:with-lock-held (*draw-font-lock*)
+    (let ((firstp t) xmin ymin xmax ymax advance-y
+          (line-height (+ (font-descent font)
+                          (font-ascent font))))
+      (flet ((process-code (code index)
+               (let ((info (font-glyph-info font code transform-glyphs)))
+                 (when firstp
+                   (setf firstp nil
+                         ymax y
+                         xmin (- x (/ line-height 2))
+                         xmax (+ x (/ line-height 2))
+                         advance-y 0))
+                 (incf advance-y (glyph-info-advance-dy info))
+                 (setf (aref (the (simple-array (unsigned-byte 32)) glyph-ids) index)
+                       (the (unsigned-byte 32) (glyph-info-id info))))))
+        (declare (inline process-code))
+        (loop with this-char = (char string start)
+              with idx0 of-type index = 0
+              for idx1 of-type index from (1+ start) below end
+              as next-char = (char string idx1)
+              as code = (char-glyph-code this-char next-char)
+              do (process-code code idx0)
+                 (setf this-char next-char)
+                 (incf idx0)
+              finally
+                 (process-code (char-code this-char) idx0)
+                 (setf ymin (+ ymax advance-y))))
+      (let ((dy (ecase align-x
+                  (:left   (- ymax ymin))
+                  (:center (/ (- ymax ymin) 2.0))
+                  (:right  0)))
+            (dx (ecase align-y
+                  (:baseline 0)
+                  (:center (- x (/ (+ xmax xmin) 2.0)))
+                  (:top    (- x xmax))
+                  (:bottom (- x xmin)))))
+        (incf x dx) (incf xmin dx) (incf xmax dx)
+        (incf y dy) (incf ymin dy) (incf ymax dy))
+      (values x y xmin ymin xmax ymax))))
