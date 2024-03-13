b4db1595d9d5865a4a7a0d832f353e1817b74056
Daniel Kochmański <daniel@turtleware.eu>
2024-03-13 12:10:23 +0100

    examples: adjust demo text transfer test
    Also change the license of the file to BSD-2-Clause.

--- Examples/text-transformation-test.lisp
+++ Examples/text-transformation-test.lisp
@@ -1,5 +1,5 @@
 ;;; ---------------------------------------------------------------------------
-;;;   License: LGPL-2.1+ (See file 'Copyright' for details).
+;;;   License: BSD-2-Clause
 ;;; ---------------------------------------------------------------------------
 ;;;
 ;;;  (c) copyright 2024 Daniel Kochmański <daniel@turtleware.eu>
@@ -12,15 +12,19 @@
 ;;; FIXME (WITH-OUTPUT-AS-GADGET TEXT-FIELD) is acting w/o incremental-redisplay.
 ;;; FIXME (WITH-OUTPUT-AS-GADGET TEXT-FIELD) cursor is reset on redisplay.
 
-(in-package #:clim-demo)
+(defpackage "CLIM-DEMO.DRAW-TEXT-TEST"
+  (:use "CLIM-LISP" "CLIM" "CLIM-EXTENSIONS")
+  (:export "DRAW-TEXT-TEST"))
+
+(in-package "CLIM-DEMO.DRAW-TEXT-TEST")
 
 (defparameter *canvas* (make-rectangle* 50 220 1230 680))
 
 (defparameter *ltr-text* "A quick brown fox jumps over the lazy dog.")
 (defparameter *rtl-text* "א  בְּרֵאשִׁית, בָּרָא אֱלֹהִים, אֵת הַשָּׁמַיִם, וְאֵת הָאָרֶץ.") ; gensis
 
-(defparameter *ttb-text* "あなたは美しいです。あなたは素晴らしいです。") ;jp
-(defparameter *ttb-text* "国际化活动") ;ch
+(defparameter *ttb-text-jp* "あなたは美しいです。あなたは素晴らしいです。") ;jp
+(defparameter *ttb-text-ch* "国际化活动") ;ch
 
 ;;; Bidirectional text with the initial direction left-to-right without and with
 ;;; right-to-left mark (rlm):
@@ -32,7 +36,18 @@
 (defparameter *bidi-rtl* "لغة C++ هي لغة برمجة تستخدم...")
 (defparameter *bidi-rtl+lrm* "لغة C++‎ هي لغة برمجة تستخدم...")
 
-(defparameter *neutral* "█0123456789")
+(defparameter *hellojp* "こんにちは123")
+(defparameter *neutral* "0123456789")
+
+(defparameter *my-text-style*
+  (make-text-style :serif :roman :large))
+
+#+ (or)
+(progn
+  (defparameter *font-path*
+    #P"~/Documents/Sync/back/Repositories/cl-dejavu/Hanza/HanaMinA.ttf")
+  (defparameter *my-text-style*
+    (clim:make-device-font-text-style (clim:find-port) (list *font-path* 36))))
 
 (define-application-frame draw-text-test ()
   ((coords :accessor coords :initform '(400 400 600 400))
@@ -119,14 +134,15 @@
   (draw-text* stream string x0 y0 :toward-x x1 :toward-y y1
                                   :align-x (align-x frame)
                                   :align-y (align-y frame)
-                                  :text-size :large
-                                  :text-family :sans-serif
+                                  :text-style *my-text-style*
                                   :transform-glyphs (transform-glyphs frame))
-  (draw-arrow* stream x0 y0 x1 y1 :ink (compose-in +dark-red+ (make-opacity .5))
-                                  :line-thickness 3
-                                  :line-dashes nil
-                                  :head-filled t :head-length 20
-                                  :head-width 10))
+  (with-drawing-options (stream :line-thickness 3 :line-dashes nil
+                                :ink (compose-in +dark-red+ (make-opacity .5)))
+    (let ((dx (- x1 x0))
+          (dy (- y1 y0)))
+      (draw-line* stream (- x0 dx) (- y0 dy) x0 y0)
+      (draw-line* stream (- x0 dy) (+ y0 dx) (+ x0 dy) (- y0 dx)))
+    (draw-arrow* stream x0 y0 x1 y1 :head-filled t :head-length 20 :head-width 10)))
 
 (defun draw-compas (frame stream ink cx cy)
   (declare (ignore frame))
