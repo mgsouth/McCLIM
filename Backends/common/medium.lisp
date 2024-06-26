482dbbbd76da4755ab196fa180746bd26bb12098
Daniel Kochmański <daniel@turtleware.eu>
2024-06-26 11:01:15 +0200

    multiline-medium-mixin: be more frugal with computations

--- Backends/common/medium.lisp
+++ Backends/common/medium.lisp
@@ -84,25 +84,30 @@
                                       start end
                                       align-x align-y toward-x toward-y
                                       transform-glyphs)
-  (let* ((base-transf (medium-device-transformation medium))
-         (text-transf (if transform-glyphs
-                          (draw-text-rotation* x y toward-x toward-y)
-                          (with-transformed-positions* (base-transf x y toward-x toward-y)
-                            (compose-transformations
-                             (draw-text-rotation* x y toward-x toward-y)
-                             (invert-transformation base-transf))))))
-    (flet ((advance-line (idx0 idx1)
-             (multiple-value-bind (w h dx dy baseline)
-                 ;; IDX1 is a position of #\newline hence #'1+
-                 (text-size medium string :start idx0 :end (1+ idx1))
-               (declare (ignore w h baseline))
-               (with-transformed-distance (text-transf dx dy)
-                 (incf x dx) (incf toward-x dx)
-                 (incf y dy) (incf toward-y dy)))))
+  (let (dx dy)
+    (flet ((advance-line ()
+             (unless dx
+               (let* ((base-transf (medium-device-transformation medium))
+                      (text-transf (if transform-glyphs
+                                       (draw-text-rotation* x y toward-x toward-y)
+                                       (with-transformed-positions*
+                                           (base-transf x y toward-x toward-y)
+                                         (compose-transformations
+                                          (draw-text-rotation* x y toward-x toward-y)
+                                          (invert-transformation base-transf))))))
+                (multiple-value-bind (w h line-dx line-dy baseline)
+                    (text-size medium #.(format nil " ~%"))
+                  ;;      ;; IDX1 is a position of #\newline hence #'1+
+                  #+ (or) (text-size medium string :start idx0 :end (1+ idx1))
+                  (declare (ignore w h baseline))
+                  (multiple-value-setq (dx dy)
+                    (transform-distance text-transf line-dx line-dy)))))
+             (incf x dx) (incf toward-x dx)
+             (incf y dy) (incf toward-y dy)))
      (loop for idx0 = start then (1+ idx1)
            for idx1 = (position #\newline string :start idx0 :end end)
            do (call-next-method medium string x y idx0 (or idx1 end)
                                 align-x align-y toward-x toward-y
                                 transform-glyphs)
            while idx1
-           do (advance-line idx0 idx1)))))
+           do (advance-line)))))
